58a5d337ea95cb148df280c03982ceaad9e4e86c
juanmedina1166 <juanesteban0901@gmail.com>
2025-10-13 10:33:41 -0500

    mecha

--- Tejo_Roguelite/Assets/Scripts/Mecha.cs
+++ Tejo_Roguelite/Assets/Scripts/Mecha.cs
@@ -1,16 +1,31 @@
+// Archivo: Mecha.cs
 using UnityEngine;
 
 public class Mecha : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    [Header("Configuración de Puntos")]
+    [SerializeField] private int puntosBase = 6; // Puntos estándar por una mecha
 
-    // Update is called once per frame
-    void Update()
+    private bool haSidoGolpeada = false;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        // Si ya fue golpeada en un frame anterior, no hagas nada.
+        if (haSidoGolpeada) return;
+
+        // Comprobamos si el objeto que nos golpeó tiene la etiqueta "Tejo".
+        // ¡Es importante que tu prefab del tejo tenga esta etiqueta!
+        if (collision.gameObject.CompareTag("Tejo"))
+        {
+            haSidoGolpeada = true;
+            Debug.Log("¡Mecha golpeada por un Tejo!");
+
+            // Lanzamos el evento global, avisando que se anotaron 'puntosBase'.
+            GameEvents.TriggerMechaExploded(puntosBase);
+
+            // Opcional: Desactivamos la mecha para que no pueda ser golpeada de nuevo.
+            // Podrías reemplazar esto con una animación de explosión y luego destruir el objeto.
+            gameObject.SetActive(false);
+        }
     }
 }
