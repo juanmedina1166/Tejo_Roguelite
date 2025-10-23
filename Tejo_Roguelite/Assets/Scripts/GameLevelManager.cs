6a747855e7e7fe78c54b40257ddc12fc622006b0
LS1653 <estebanpuerta2006@gmail.com>
2025-10-22 22:14:22 -0500

    corrección-dificultad-segun-el-nivel

--- Tejo_Roguelite/Assets/Scripts/GameLevelManager.cs
+++ Tejo_Roguelite/Assets/Scripts/GameLevelManager.cs
@@ -6,7 +6,6 @@ public class GameLevelManager : MonoBehaviour
 
     [Range(0, 5)]
     public int nivelActual = 0; // 0 = Tutorial, 1–5 = Partidas normales
-    public DificultadIA dificultadActual; // Guardará la dificultad elegida en cada nivel
 
     void Awake()
     {
@@ -16,29 +15,33 @@ public class GameLevelManager : MonoBehaviour
             Destroy(gameObject);
     }
 
-    private void Start()
-    {
-        // Determinar la dificultad al inicio del juego
-        dificultadActual = ElegirDificultad();
-    }
-
     public PersonajeIAData ObtenerDatosIA()
     {
+        DificultadIA dificultad = DeterminarDificultadSegunNivel(nivelActual);
+
         switch (nivelActual)
         {
             case 0: return new PersonajeIAData("Estefa", DificultadIA.Facil);
-            case 1: return new PersonajeIAData("Brayan MID", dificultadActual);
-            case 2: return new PersonajeIAData("Valentina Vinotinto", dificultadActual);
-            case 3: return new PersonajeIAData("Don Alcides", dificultadActual);
-            case 4: return new PersonajeIAData("Jerlumine Orozco", dificultadActual);
-            case 5: return new PersonajeIAData("Doña Gertrudis", dificultadActual);
-            default: return new PersonajeIAData("Marcos Garzon", dificultadActual);
+            case 1: return new PersonajeIAData("Brayan MID", dificultad);
+            case 2: return new PersonajeIAData("Valentina Vinotinto", dificultad);
+            case 3: return new PersonajeIAData("Don Alcides", dificultad);
+            case 4: return new PersonajeIAData("Jerlumine Orozco", dificultad);
+            case 5: return new PersonajeIAData("Doña Gertrudis", dificultad);
+            default: return new PersonajeIAData("Marcos Garzon", dificultad);
         }
     }
 
-    private DificultadIA ElegirDificultad()
+    private DificultadIA DeterminarDificultadSegunNivel(int nivel)
     {
-        int rand = Random.Range(0, 3);
-        return (DificultadIA)rand; // 0 = Fácil, 1 = Normal, 2 = Difícil
+        switch (nivel)
+        {
+            case 0: return DificultadIA.Facil; // Tutorial
+            case 1: return DificultadIA.Facil;
+            case 2: return (Random.value < 0.5f) ? DificultadIA.Facil : DificultadIA.Media;
+            case 3: return DificultadIA.Media;
+            case 4: return (Random.value < 0.5f) ? DificultadIA.Media : DificultadIA.Dificil;
+            case 5: return DificultadIA.Dificil;
+            default: return DificultadIA.Facil;
+        }
     }
 }
